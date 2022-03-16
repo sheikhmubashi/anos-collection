bc46a4cda8046cf72d54d90e4f90eb7a1e8bf70e
Sheikh Mubashir <smubashir655@gmail.com>
2022-03-16 12:40:43 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import Layout from '../components/Layout';
+import db from '../utils/db';
+import Product from '../models/Product';
+import NextLink from 'next/link';
+export default function Home(props) {
+  const { products } = props;
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography>Rs:{product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
---
+++ utils/data.js
@@ -0,0 +1,123 @@
+const data = {
+  products: [
+    {
+      name: 'Women Stylsi Kurti',
+      slug: 'women-stylsih-kurti',
+      category: 'Shirts',
+      image: '/images/shirt1.jpeg',
+      price: 500,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Branded Kurti',
+      slug: 'women-branded-kurti',
+      category: 'Shirts',
+      image: '/images/shirt2.jpeg',
+      price: 700,
+      brand: 'Anos Collection',
+      rating: 2.5,
+      numReviews: 20,
+      countInStock: 10,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Kurti',
+      slug: 'women-kurti',
+      category: 'Shirts',
+      image: '/images/shirt3.jpeg',
+      price: 200,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 30,
+      countInStock: 6,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsi Shirt',
+      slug: 'women-stylsih-shirt',
+      category: 'Shirts',
+      image: '/images/shirt4.jpeg',
+      price: 900,
+      brand: 'Anos Collection',
+      rating: 3.5,
+      numReviews: 8,
+      countInStock: 2,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsi Top Kurti',
+      slug: 'women-stylsih-top-kurti',
+      category: 'Shirts',
+      image: '/images/shirt5.jpeg',
+      price: 500,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 3,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsi Top Branded Kurti',
+      slug: 'women-stylsih-top-branded-kurti',
+      category: 'Shirts',
+      image: '/images/shirt6.jpeg',
+      price: 500,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 89,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsi Shirt Kurti',
+      slug: 'women-stylsih-shirt-kurti',
+      category: 'Shirts',
+      image: '/images/shirt7.jpeg',
+      price: 500,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 19,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsi Branded Shirt Kurti',
+      slug: 'women-stylsih-branded-shirt-kurti',
+      category: 'Shirts',
+      image: '/images/shirt8.jpeg',
+      price: 500,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+    {
+      name: 'Women Stylsih Designing Kurti',
+      slug: 'women-stylsih-designing-kurti',
+      category: 'Shirts',
+      image: '/images/shirt9.jpeg',
+      price: 1000,
+      brand: 'Anos Collection',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description:
+        'Ladies Stylish Cotton Kurti – 20% OFF Branded Trendy Stylish',
+    },
+  ],
+};
+
+export default data;
