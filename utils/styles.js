6ae4c0dcace539e19beb508f6b71183bdea31982
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-01 17:33:56 +0500

    asd
    asd

--- utils/styles.js
+++ utils/styles.js
@@ -1,44 +1,44 @@
-import { makeStyles } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  navbar: {
-    backgroundColor: 'black',
-    '& a': {
-      color: '#ffffff',
-      marginLeft: 10,
-    },
-  },
-  brand: {
-    fontWeight: 'bold',
-    fontSize: '1.5rem',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  main: {
-    minHeight: '80vh',
-  },
-  footer: {
-    marginTop: 10,
-    textAlign: 'center',
-  },
-  section: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  form: {
-    maxWidth: 800,
-    margin: '0 auto',
-  },
-  navbarButton: {
-    color: '#ffffff',
-    textTransform: 'initial',
-  },
-  transparentBackgroud: {
-    backgroundColor: 'transparent',
-  },
-  error: {
-    color: '#f04040',
-  },
-});
-export default useStyles;
+// const useStyles = makeStyles({
+//   navbar: {
+//     backgroundColor: 'black',
+//     '& a': {
+//       color: '#ffffff',
+//       marginLeft: 10,
+//     },
+//   },
+//   brand: {
+//     fontWeight: 'bold',
+//     fontSize: '1.5rem',
+//   },
+//   grow: {
+//     flexGrow: 1,
+//   },
+//   main: {
+//     minHeight: '80vh',
+//   },
+//   footer: {
+//     marginTop: 10,
+//     textAlign: 'center',
+//   },
+//   section: {
+//     marginTop: 10,
+//     marginBottom: 10,
+//   },
+//   form: {
+//     maxWidth: 800,
+//     margin: '0 auto',
+//   },
+//   navbarButton: {
+//     color: '#ffffff',
+//     textTransform: 'initial',
+//   },
+//   transparentBackgroud: {
+//     backgroundColor: 'transparent',
+//   },
+//   error: {
+//     color: '#f04040',
+//   },
+// });
+// export default useStyles;
