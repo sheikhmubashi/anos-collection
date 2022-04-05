6b3b914009342172e7234610f25a497f77788019
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-05 12:59:40 +0500

    sad
    asd

--- components/Layout.js
+++ components/Layout.js
@@ -1,179 +1,179 @@
-import {
-  AppBar,
-  Container,
-  Link,
-  Toolbar,
-  Typography,
-  Badge,
-  Button,
-  Menu,
-  MenuItem,
-} from '@material-ui/core';
-import {
-  createTheme,
-  ThemeProvider,
-  CssBaseline,
-  Switch,
-} from '@material-ui/core';
-import { useContext } from 'react';
-import Cookies from 'js-cookie';
-import { Store } from '../utils/Store';
-import Head from 'next/head';
-import NextLink from 'next/link';
-import React from 'react';
-import useStyles from '../utils/styles';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-// import Image from 'next/image';
-// import logo from '../hfshoplogo.jpg'
-function Layout({ title, description, children }) {
-  const router = useRouter();
-  const { state, dispatch } = useContext(Store);
-  const { darkMode, cart, userInfo } = state;
-  const classes = useStyles();
-  const darkModeChangeHandler = () => {
-    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
-    const newDarkMode = !darkMode;
-    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
-  };
-  const [anchorEl, setAnchorEl] = useState(null);
-  const loginClickHandler = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const loginMenuCloseHandler = (e, redirect) => {
-    setAnchorEl(null);
-    if (redirect) {
-      router.push(redirect);
-    }
-  };
-  const logoutClickHandler = () => {
-    setAnchorEl(null);
-    dispatch({ type: 'USER_LOGOUT' });
-    Cookies.remove('userInfo');
-    Cookies.remove('cartItems');
-    router.push('/');
-  };
-  const theme = createTheme({
-    typography: {
-      h1: {
-        fontSize: '1.6rem',
-        fontWeight: 400,
-        margin: '1rem 0',
-      },
-      h2: {
-        fontSize: '1.4rem',
-        fontWeight: 400,
-        margin: '1rem 0',
-      },
-    },
-    palette: {
-      type: darkMode ? 'dark' : 'light',
-      primary: {
-        main: '#f0c000',
-      },
-      secondary: {
-        main: '#208080',
-      },
-    },
-  });
-  return (
-    <div>
-      <Head>
-        <title>
-          {title ? `${title} - Anos Collection` : 'Anos Collection'}
-        </title>
-        {description && <meta name="description" content={description}></meta>}
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="static" className={classes.navbar}>
-          <Toolbar>
-            <NextLink href="/" passHref>
-              <Link>
-                <Typography className={classes.brand}>
-                  Anos Collection
-                </Typography>
-                {/* <Typography className={classes.brand}>
-                  <Image src={logo} alt="logo" width={"200"} height={"60"} />
-                </Typography> */}
-              </Link>
-            </NextLink>
-            <div className={classes.grow}></div>
-            <div>
-              <Switch
-                checked={darkMode}
-                onChange={darkModeChangeHandler}
-              ></Switch>
-              <NextLink href="/cart" passHref>
-                <Link>
-                  {cart.cartItems.length > 0 ? (
-                    <Badge
-                      color="secondary"
-                      badgeContent={cart.cartItems.length}
-                    >
-                      Cart
-                    </Badge>
-                  ) : (
-                    'Cart'
-                  )}
-                </Link>
-              </NextLink>
-              {userInfo ? (
-                <>
-                  <Button
-                    aria-controls="simple-menu"
-                    aria-haspopup="true"
-                    onClick={loginClickHandler}
-                    className={classes.navbarButton}
-                  >
-                    {userInfo.name}
-                  </Button>
-                  <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={loginMenuCloseHandler}
-                  >
-                    <MenuItem
-                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}
-                    >
-                      Profile
-                    </MenuItem>
-                    <MenuItem
-                      onClick={(e) =>
-                        loginMenuCloseHandler(e, '/order-history')
-                      }
-                    >
-                      Order Hisotry
-                    </MenuItem>
-                    {userInfo.isAdmin && (
-                      <MenuItem
-                        onClick={(e) =>
-                          loginMenuCloseHandler(e, '/admin/dashboard')
-                        }
-                      >
-                        Admin Dashboard
-                      </MenuItem>
-                    )}
-                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
-                  </Menu>
-                </>
-              ) : (
-                <NextLink href="/login" passHref>
-                  <Link>Login</Link>
-                </NextLink>
-              )}
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Container className={classes.main}>{children}</Container>
-        <footer className={classes.footer}>
-          <Typography>All rights reserved. Anos Collection.</Typography>
-        </footer>
-      </ThemeProvider>
-    </div>
-  );
-}
+// import {
+//   AppBar,
+//   Container,
+//   Link,
+//   Toolbar,
+//   Typography,
+//   Badge,
+//   Button,
+//   Menu,
+//   MenuItem,
+// } from '@material-ui/core';
+// import {
+//   createTheme,
+//   ThemeProvider,
+//   CssBaseline,
+//   Switch,
+// } from '@material-ui/core';
+// import { useContext } from 'react';
+// import Cookies from 'js-cookie';
+// import { Store } from '../utils/Store';
+// import Head from 'next/head';
+// import NextLink from 'next/link';
+// import React from 'react';
+// import useStyles from '../utils/styles';
+// import { useState } from 'react';
+// import { useRouter } from 'next/router';
+// // import Image from 'next/image';
+// // import logo from '../hfshoplogo.jpg'
+// function Layout({ title, description, children }) {
+//   const router = useRouter();
+//   const { state, dispatch } = useContext(Store);
+//   const { darkMode, cart, userInfo } = state;
+//   const classes = useStyles();
+//   const darkModeChangeHandler = () => {
+//     dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+//     const newDarkMode = !darkMode;
+//     Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+//   };
+//   const [anchorEl, setAnchorEl] = useState(null);
+//   const loginClickHandler = (e) => {
+//     setAnchorEl(e.currentTarget);
+//   };
+//   const loginMenuCloseHandler = (e, redirect) => {
+//     setAnchorEl(null);
+//     if (redirect) {
+//       router.push(redirect);
+//     }
+//   };
+//   const logoutClickHandler = () => {
+//     setAnchorEl(null);
+//     dispatch({ type: 'USER_LOGOUT' });
+//     Cookies.remove('userInfo');
+//     Cookies.remove('cartItems');
+//     router.push('/');
+//   };
+//   const theme = createTheme({
+//     typography: {
+//       h1: {
+//         fontSize: '1.6rem',
+//         fontWeight: 400,
+//         margin: '1rem 0',
+//       },
+//       h2: {
+//         fontSize: '1.4rem',
+//         fontWeight: 400,
+//         margin: '1rem 0',
+//       },
+//     },
+//     palette: {
+//       type: darkMode ? 'dark' : 'light',
+//       primary: {
+//         main: '#f0c000',
+//       },
+//       secondary: {
+//         main: '#208080',
+//       },
+//     },
+//   });
+//   return (
+//     <div>
+//       <Head>
+//         <title>
+//           {title ? `${title} - Anos Collection` : 'Anos Collection'}
+//         </title>
+//         {description && <meta name="description" content={description}></meta>}
+//       </Head>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <AppBar position="static" className={classes.navbar}>
+//           <Toolbar>
+//             <NextLink href="/" passHref>
+//               <Link>
+//                 <Typography className={classes.brand}>
+//                   Anos Collection
+//                 </Typography>
+//                 {/* <Typography className={classes.brand}>
+//                   <Image src={logo} alt="logo" width={"200"} height={"60"} />
+//                 </Typography> */}
+//               </Link>
+//             </NextLink>
+//             <div className={classes.grow}></div>
+//             <div>
+//               <Switch
+//                 checked={darkMode}
+//                 onChange={darkModeChangeHandler}
+//               ></Switch>
+//               <NextLink href="/cart" passHref>
+//                 <Link>
+//                   {cart.cartItems.length > 0 ? (
+//                     <Badge
+//                       color="secondary"
+//                       badgeContent={cart.cartItems.length}
+//                     >
+//                       Cart
+//                     </Badge>
+//                   ) : (
+//                     'Cart'
+//                   )}
+//                 </Link>
+//               </NextLink>
+//               {userInfo ? (
+//                 <>
+//                   <Button
+//                     aria-controls="simple-menu"
+//                     aria-haspopup="true"
+//                     onClick={loginClickHandler}
+//                     className={classes.navbarButton}
+//                   >
+//                     {userInfo.name}
+//                   </Button>
+//                   <Menu
+//                     id="simple-menu"
+//                     anchorEl={anchorEl}
+//                     keepMounted
+//                     open={Boolean(anchorEl)}
+//                     onClose={loginMenuCloseHandler}
+//                   >
+//                     <MenuItem
+//                       onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+//                     >
+//                       Profile
+//                     </MenuItem>
+//                     <MenuItem
+//                       onClick={(e) =>
+//                         loginMenuCloseHandler(e, '/order-history')
+//                       }
+//                     >
+//                       Order Hisotry
+//                     </MenuItem>
+//                     {userInfo.isAdmin && (
+//                       <MenuItem
+//                         onClick={(e) =>
+//                           loginMenuCloseHandler(e, '/admin/dashboard')
+//                         }
+//                       >
+//                         Admin Dashboard
+//                       </MenuItem>
+//                     )}
+//                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+//                   </Menu>
+//                 </>
+//               ) : (
+//                 <NextLink href="/login" passHref>
+//                   <Link>Login</Link>
+//                 </NextLink>
+//               )}
+//             </div>
+//           </Toolbar>
+//         </AppBar>
+//         <Container className={classes.main}>{children}</Container>
+//         <footer className={classes.footer}>
+//           <Typography>All rights reserved. Anos Collection.</Typography>
+//         </footer>
+//       </ThemeProvider>
+//     </div>
+//   );
+// }
 
-export default Layout;
+// export default Layout;
