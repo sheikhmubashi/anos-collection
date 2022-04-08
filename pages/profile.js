c0125188a94aab7ca78b2b435035e3687d583d46
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-08 16:26:43 +0500

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -218,7 +218,7 @@ function Profile() {
                                                 variant="contained"
                                                 type="submit"
                                                 fullWidth
-                                                color="primary"
+                                                color="primary" 
                                             >
                                                 Update
                                             </Button>
