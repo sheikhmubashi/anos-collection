1729d67623972b408b3de15d50eccd359be9a95e
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-09 12:35:15 +0500

    completed edit & delete page

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -12,10 +12,10 @@ handler.get(async (req, res) => {
     await db.disconnect();
     res.send(product);
 });
-handler.put(async (req, res) => { 
+handler.put(async (req, res) => {
     await db.connect();
     const product = await Product.findById(req.query.id);
-    if (product) {
+    if (product) {  
         product.name = req.body.name;
         product.slug = req.body.slug;
         product.price = req.body.price;
@@ -32,4 +32,17 @@ handler.put(async (req, res) => {
         res.status(404).send({ message: 'Product Not Found' });
     }
 });
+handler.delete(async (req, res) => {
+    await db.connect();
+    const product = await Product.findById(req.query.id);
+    if (product) {
+        await product.remove();
+        await db.disconnect();
+        res.send({ message: 'Product Deleted' });
+    } else {
+        await db.disconnect();
+        res.status(404).send({ message: 'Product Not Found' });
+    }
+});
+
 export default handler;
