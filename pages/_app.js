f130a1aa695444687354150577edb5066ef30bee
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-13 16:27:43 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement.removeChild(jssStyles); 
     }
   }, []);
   return (
