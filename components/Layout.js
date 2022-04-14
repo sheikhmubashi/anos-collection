ef919c72bf790a67dcebd8f8bb4164cb1ac3a4da
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-14 16:04:25 +0500

    search input completed

--- components/Layout.js
+++ components/Layout.js
@@ -15,9 +15,11 @@ import {
   ListItem,
   Divider,
   ListItemText,
+  InputBase,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import CancelIcon from '@material-ui/icons/Cancel';
+import SearchIcon from '@material-ui/icons/Search';
 import {
   createMuiTheme,
   ThemeProvider,
@@ -63,6 +65,14 @@ function Layout({ title, description, children }) {
     } catch (err) {
       enqueueSnackbar(getError(err), { variant: 'error' });
     }
+  };
+  const [query, setQuery] = useState('');
+  const queryChangeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push(`/search?query=${query}`);
   };
   useEffect(() => {
     fetchCategories();
@@ -130,6 +140,7 @@ function Layout({ title, description, children }) {
                 edge="start"
                 aria-label="open drawer"
                 onClick={sidebarOpenHandler}
+                className={classes.menuButton}
               >
                 <MenuIcon className={classes.navbarButton} />
               </IconButton>
@@ -178,7 +189,23 @@ function Layout({ title, description, children }) {
                 ))}
               </List>
             </Drawer>
-            <div className={classes.grow}></div>
+            <div className={classes.searchSection}>
+              <form onSubmit={submitHandler} className={classes.searchForm}>
+                <InputBase
+                  name="query"
+                  className={classes.searchInput}
+                  placeholder="Search products"
+                  onChange={queryChangeHandler}
+                />
+                <IconButton
+                  type="submit"
+                  className={classes.iconButton}
+                  aria-label="search"
+                >
+                  <SearchIcon />
+                </IconButton>
+              </form>
+            </div>
             <div>
               <Switch
                 checked={darkMode}
