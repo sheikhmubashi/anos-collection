de5aef5d606d36571f55f71a15575c2375072d51
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-18 16:16:53 +0500

    complted crousel

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -8,19 +8,21 @@ handler.use(isAuth, isAdmin);
 
 handler.get(async (req, res) => {
     await db.connect();
-    const product = await Product.findById(req.query.id); 
+    const product = await Product.findById(req.query.id);
     await db.disconnect();
-    res.send(product); 
+    res.send(product);
 });
 handler.put(async (req, res) => {
     await db.connect();
     const product = await Product.findById(req.query.id);
-    if (product) {  
+    if (product) {
         product.name = req.body.name;
         product.slug = req.body.slug;
         product.price = req.body.price;
         product.category = req.body.category;
         product.image = req.body.image;
+        product.featuredImage = req.body.featuredImage;
+        product.isFeatured = req.body.isFeatured;
         product.brand = req.body.brand;
         product.countInStock = req.body.countInStock;
         product.description = req.body.description;
