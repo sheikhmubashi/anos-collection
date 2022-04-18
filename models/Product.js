88884c2c3e5d05f18b6d6f75dcee0a6b17dca9c3
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-18 16:19:35 +0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -25,7 +25,7 @@ const productSchema = new mongoose.Schema(
         countInStock: { type: Number, required: true, default: 0 },
         description: { type: String, required: true },
         reviews: [reviewSchema],
-        featuredImage: { type: String },
+        featuredImage: { type: String }, 
         isFeatured: { type: Boolean, required: true, default: false },
     },
     {
