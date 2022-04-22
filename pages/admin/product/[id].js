46a5957592b46d39e86e7e4da85b20ae1bfa8328
Sheikh Mubashir <smubashir655@gmail.com>
2022-04-22 16:01:32 +0500

    Update [id].js

--- pages/admin/product/[id].js
+++ pages/admin/product/[id].js
@@ -27,7 +27,7 @@ function reducer(state, action) {
     switch (action.type) {
         case 'FETCH_REQUEST':
             return { ...state, loading: true, error: '' };
-        case 'FETCH_SUCCESS':
+        case 'FETCH_SUCCESS': 
             return { ...state, loading: false, error: '' };
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload };
