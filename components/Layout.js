c25b25f3bb4e623c39a2eb354e8324b63ac3779e
Sheikh Mubashir <smubashir655@gmail.com>
2022-05-23 14:39:03 +0500

    footer css

--- components/Layout.js
+++ components/Layout.js
@@ -317,17 +317,17 @@ function Layout({ title, description, children }) {
         </AppBar>
         <Container className={classes.main}>{children}</Container>
         <footer className="layOutFooter" style={{ "backgroundColor": "black", "display": "flex", 'justifyContent': 'space-evenly' }}>
-          <div className='footerDiv' style={{ 'color': 'white'}}>
+          <div className='footerDivOne'>
             <h1>About Us</h1>
             <p>ANOS mission is to be world’s most customer centric company, to establish a place where customers can come to find different variety they might want to buy online. hfshop.com.pk caters best quality clothing, accessories, makeup and household items at most affordable price. We aim to provide the best possible online shopping experience to our customers. Shop at hfshop.com.pk and avail the best discounts & offers on your favorite products.</p>
           </div>
-          <div className='footerDiv' style={{ 'color': 'white' }}>
+          <div  style={{ 'color': 'white' }}>
             <h1>FOLLOW US</h1>
             <p>Facebook</p>
             <p>Twitter</p>
             <p>Instagram</p>
           </div>
-          <div className='footerDiv' style={{ 'color': 'white' }}>
+          <div style={{ 'color': 'white' }}>
             <h1>Payment Method</h1>
             <p>Cash on delivery</p>
             <p>Bank transfer</p>
